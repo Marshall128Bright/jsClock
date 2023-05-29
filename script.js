b1e7aa7bf4e965b291414d1c4219bdd637a12d8b
Marshall128Bright <chavalalabright@gmail.com>
2023-05-29 13:11:27 +0200

    wrote the js logic of the clock

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function run(){
-    
+    setInterval(()=>{
+        const time = new Date()
+        document.getElementById("clock").innerHTML = `${time.getHours()} : ${time.getMinutes()}.${time.getSeconds()}` 
+    },1000)
 }
